d0229b6b2d33e932aef73807a7511d09dcffc6e7
Kenji Nagahashi <kenji@archive.org>
2014-02-19 23:37:44 +0000

    cleanup setup.py indent for ease of add/remove things. also use find_package().

--- setup.py
+++ setup.py
@@ -1,22 +1,41 @@
 #!/usr/bin/env python
 # vim: set sw=4 et:
 
-import setuptools
+from setuptools import setup, find_packages
 import glob
 
-setuptools.setup(name='pywb',
-        version='0.2',
-        url='https://github.com/ikreymer/pywb',
-        author='Ilya Kreymer',
-        author_email='ilya@archive.org',
-        long_description=open('README.md').read(),
-        license='GPL',
-        packages=['pywb','pywb.utils','pywb.cdx','pywb.warc','pywb.rewrite'],
-        provides=['pywb','pywb.utils','pywb.cdx','pywb.warc','pywb.rewrite'],
-        package_data={'pywb': ['ui/*', 'static/*'], 'pywb.cdx': ['*.yaml']},
-        data_files = [('sample_archive/cdx/', glob.glob('sample_archive/cdx/*')),
-                      ('sample_archive/warcs/', glob.glob('sample_archive/warcs/*'))],
-        install_requires=['uwsgi', 'rfc3987', 'chardet', 'redis', 'jinja2', 'surt', 'pyyaml', 'WebTest','pytest'],
-#        tests_require=['WebTest', 'pytest'],
-        zip_safe=False)
+setup(
+    name='pywb',
+    version='0.2',
+    url='https://github.com/ikreymer/pywb',
+    author='Ilya Kreymer',
+    author_email='ilya@archive.org',
+    long_description=open('README.md').read(),
+    license='GPL',
+    packages=find_packages(),
+    provides=[
+        'pywb','pywb.utils','pywb.cdx','pywb.warc','pywb.rewrite'
+        ],
+    package_data={
+        'pywb': ['ui/*', 'static/*'],
+        'pywb.cdx': ['*.yaml']
+        },
+    data_files = [
+        ('sample_archive/cdx/', glob.glob('sample_archive/cdx/*')),
+        ('sample_archive/warcs/', glob.glob('sample_archive/warcs/*'))
+        ],
+    install_requires=[
+        'uwsgi',
+        'rfc3987',
+        'chardet',
+        'redis',
+        'jinja2',
+        'surt',
+        'pyyaml',
+        'WebTest',
+        'pytest',
+        ],
+    # tests_require=['WebTest', 'pytest'],
+    zip_safe=False
+    )
 
