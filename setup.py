9be70741836ff09178b236614a7bb6d76eb6d89d
Ilya Kreymer <ikreymer@gmail.com>
2014-09-07 11:58:03 -0700

    bump version to 0.6.1 fix small typo in cert_download for not-available message, spacing in config.yaml

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ class PyTest(TestCommand):
 
 setup(
     name='pywb',
-    version='0.6.0',
+    version='0.6.1',
     url='https://github.com/ikreymer/pywb',
     author='Ilya Kreymer',
     author_email='ikreymer@gmail.com',
