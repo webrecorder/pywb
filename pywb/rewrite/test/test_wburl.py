1286e9da4c9d6e03179f688b612381134bf7d225
Ilya Kreymer <ikreymer@gmail.com>
2015-04-04 17:31:38 -0700

    wburl test: better test for bad domain

--- pywb/rewrite/test/test_wburl.py
+++ pywb/rewrite/test/test_wburl.py
@@ -109,7 +109,7 @@ http://d0%D1%80%D0%B8%D0%BC%D0%B5%D1%80.%D0%B8%D1%81%D0%BF%D1%8B%D1%82%D0%B0%D0%
 http://xn--abcd
 
 # some bizzare input, ensure exception is caught
->>> print(WbUrl.to_uri('%20' * 25))
+>>> print(WbUrl.to_uri('%20' * 100))
 <BLANKLINE>
 
 # IRI representation
