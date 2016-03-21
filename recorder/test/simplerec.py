aa80cd6881169f1cf60fd4caee0347728284ceb3
Ilya Kreymer <ikreymer@gmail.com>
2016-03-21 11:50:01 -0700

    recorder: add simple recorder config indexing to redis

---
+++ recorder/test/simplerec.py
@@ -0,0 +1,23 @@
+from gevent import monkey; monkey.patch_all()
+
+from recorder.recorderapp import RecorderApp
+from recorder.redisindexer import WritableRedisIndexer
+
+from recorder.warcwriter import MultiFileWARCWriter
+from recorder.filters import SkipDupePolicy
+
+upstream_url = 'http://localhost:8080'
+
+target = './_recordings/'
+
+dedup_index = WritableRedisIndexer('redis://localhost/2/rec:cdxj',
+                file_key_template='rec:warc',
+                rel_path_template=target,
+                dupe_policy=SkipDupePolicy())
+
+recorder_app = RecorderApp(upstream_url,
+                MultiFileWARCWriter(target, dedup_index=dedup_index),
+                 accept_colls='live')
+
+application = recorder_app
+
