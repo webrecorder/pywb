4b37d13c34369c55b875c89ae658b6bd7b7effeb
Ilya Kreymer <ikreymer@gmail.com>
2016-03-20 22:23:07 -0700

    tests/gevent: skip gevent monkey patch when running tests

--- pywb/apps/wayback.py
+++ pywb/apps/wayback.py
@@ -1,8 +1,11 @@
-# Use gevent if available
-try:
-    from gevent.monkey import patch_all; patch_all()
-except Exception as e:
-    pass
+import sys
+
+if not hasattr(sys, '_called_from_test'):  #pragma: no cover
+    # Use gevent if available
+    try:
+        from gevent.monkey import patch_all; patch_all()
+    except Exception as e:
+        pass
 
 from pywb.framework.wsgi_wrappers import init_app
 from pywb.webapp.pywb_init import create_wb_router
