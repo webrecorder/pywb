b68ec9b9c7477ce62f5aa071398d62db9bccaa29
Ilya Kreymer <ikreymer@gmail.com>
2017-03-15 11:15:20 -0700

    html rewriter: srcset rewrite: don't add extra space

--- pywb/rewrite/html_rewriter.py
+++ pywb/rewrite/html_rewriter.py
@@ -220,7 +220,7 @@ class HTMLRewriterMixin(object):
 
         values = value.split(',')
         values = [self._rewrite_url(v.strip()) for v in values]
-        return ', '.join(values)
+        return ','.join(values)
 
     def _rewrite_css(self, css_content):
         if css_content:
