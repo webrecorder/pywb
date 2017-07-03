41b378941207a2a703394788c38b56b9fec5038a
Ilya Kreymer <ikreymer@gmail.com>
2017-07-03 09:01:21 -0700

    update to warcio>=1.3.4 http adapter: use same defaults for live and remote

--- pywb/warcserver/http.py
+++ pywb/warcserver/http.py
@@ -2,6 +2,6 @@ from requests.adapters import HTTPAdapter
 
 class DefaultAdapters(object):
     live_adapter = HTTPAdapter(max_retries=3)
-    remote_adapter = HTTPAdapter(pool_connections=8, pool_maxsize=8, pool_block=True)
+    remote_adapter = HTTPAdapter(max_retries=3)
 
 
