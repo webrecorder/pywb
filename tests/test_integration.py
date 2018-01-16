93173fa56f874720abc70675e041dd0c739bd0c1
Ilya Kreymer <ikreymer@gmail.com>
2018-01-15 19:01:28 -0800

    tests: remove content-type assert due to platform differences

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -400,7 +400,6 @@ class TestWbIntegration(BaseConfigTest):
     def test_static_nested_dir(self):
         resp = self.testapp.get('/static/fonts/font-awesome/fontawesome-webfont.woff')
         assert resp.status_int == 200
-        assert resp.content_type == 'application/font-woff'
         assert resp.content_length > 0
 
     def test_static_not_found(self):
