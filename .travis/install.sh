2f062cf5c73097a19599b7f9c11dafe854d0f5c8
John Berlin <n0tan3rd@gmail.com>
2018-07-09 16:21:14 -0400

    New integration tests using webrecorder-tests: (#355)
    New integration tests using webrecorder-tests:
    - WR_TEST=true is set for integration test run (only run with py3.6, excluded for py2.7, 3.5)
    - Added .travis directory that includes two scripts: install.sh and test.sh.
    - install.sh handles all installation and test.sh handles running of unit or integration tests
    - sudo: true required to run headless chrome

---
+++ .travis/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+python setup.py -q install
+pip install -r extra_requirements.txt
+pip install coverage pytest-cov coveralls
+pip install codecov
+npm install
+
+if [ "$WR_TEST" = "yes" ]; then
+    git clone https://github.com/webrecorder/webrecorder-tests.git
+    cd webrecorder-tests
+    pip install --upgrade -r requirements.txt
+    ./bootstrap.sh
+fi
