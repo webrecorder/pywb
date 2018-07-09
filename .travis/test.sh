a4e296f3b6dde51582171ba738d052edc2189372
John Berlin <n0tan3rd@gmail.com>
2018-07-09 15:24:17 -0400

    New integration tests using webrecorder-tests: - WR_TEST=true is set for integration test run (only run with py3.6, excluded for py2.7, 3.5) - Added .travis directory that includes two scripts: install.sh and test.sh. - install.sh - handles all installation and test.sh handles running of unit or integration tests - sudo: true required to run headless chrome

---
+++ .travis/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+if [ "$WR_TEST" = "no" ]; then
+    python setup.py test
+    cd karma-tests && make test && cd ..
+else
+    cd webrecorder-tests
+    INTRAVIS=1 pytest -m "pywbtest"
+fi
