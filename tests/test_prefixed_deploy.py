25aa4aa5192e7de390c27add82128bbaf613e533
Ilya Kreymer <ikreymer@gmail.com>
2018-08-23 18:18:47 -0700

    non-root deployment and static prefix: (ported from uk-pywb fork) - store original wsgi SCRIPT_NAME (before collection path is pushed) in 'pywb.app_prefix' env var - set 'pywb.host_prefix' via rewriterapp - add 'static_prefix' jinja env global which defaults to 'pywb.host_prefix + pywb.app_prefix + static/' - set 'static_prefix' to absolute url if available (to support proxy mode) - update existing templates to use '{{ static_prefix }}' instead of '{{ host_prefix }}/{{ static_path }' - update index.html to use pywb.app_prefix for collection links - tests: add test_prefixed_deploy.py to ensure all paths are prefixed as expected

---
+++ tests/test_prefixed_deploy.py
@@ -0,0 +1,50 @@
+from .base_config_test import BaseConfigTest, fmod
+
+
+# ============================================================================
+class TestPrefixedDeploy(BaseConfigTest):
+    @classmethod
+    def setup_class(cls):
+        super(TestPrefixedDeploy, cls).setup_class('config_test.yaml')
+
+    def get(self, url, fmod=''):
+        return super(TestPrefixedDeploy, self).get(url, fmod,
+                        extra_environ={'SCRIPT_NAME': '/prefix',
+                                       'REQUEST_URI': 'http://localhost:80/prefix' + url})
+
+    def test_home(self):
+        resp = self.get('/prefix/')
+        print(resp.text)
+        self._assert_basic_html(resp)
+        assert '/prefix/pywb' in resp.text
+
+    def test_pywb_root(self):
+        resp = self.get('/prefix/pywb/')
+        self._assert_basic_html(resp)
+        assert 'Search' in resp.text
+
+    def test_calendar_query(self):
+        resp = self.get('/prefix/pywb/*/iana.org')
+        self._assert_basic_html(resp)
+
+        assert '/prefix/static/query.js' in resp.text
+
+    def test_replay_content(self, fmod):
+        resp = self.get('/prefix/pywb/20140127171238{0}/http://www.iana.org/', fmod)
+        self._assert_basic_html(resp)
+
+        assert '"20140127171238"' in resp.text, resp.text
+        assert "'http://localhost:80/prefix/static/wombat.js'" in resp.text
+        assert "'http://localhost:80/prefix/static/default_banner.js'" in resp.text
+        assert '"http://localhost:80/prefix/static/"' in resp.text
+        assert '"http://localhost:80/prefix/pywb/"' in resp.text
+        assert 'new _WBWombat' in resp.text, resp.text
+        assert '"/prefix/pywb/20140127171238{0}/http://www.iana.org/time-zones"'.format(fmod) in resp.text, resp.text
+
+    def test_static_content(self):
+        resp = self.get('/prefix/static/default_banner.css')
+        assert resp.status_int == 200
+        assert resp.content_type == 'text/css'
+        assert resp.content_length > 0
+
+
