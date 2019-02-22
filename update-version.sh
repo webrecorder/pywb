fb41d2034b71f71b903ce5f4403b7b490aa38cef
Ilya Kreymer <ikreymer@gmail.com>
2019-02-21 23:13:00 -0800

    use tags instead of branch

--- update-version.sh
+++ update-version.sh
@@ -3,6 +3,6 @@
 BASE=2.2
 NOW=$(date +%Y%m%d)
 sed -i='' -E  "s/(__version__ = ').*$/\1$BASE.$NOW'/" ./pywb/version.py 
-git checkout -b v-$BASE.$NOW
+git tag v-$BASE.$NOW
 git commit ./pywb/version.py
 git push origin v-$BASE.$NOW
