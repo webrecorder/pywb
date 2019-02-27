24f92054d92fcb0bd6c46e68bebb1a3d20e75c22
Ilya Kreymer <ikreymer@gmail.com>
2019-02-27 15:51:02 -0800

    versioning: update version update script to include push, commit message

--- update-version.sh
+++ update-version.sh
@@ -4,5 +4,6 @@ BASE=2.2
 NOW=$(date +%Y%m%d)
 sed -i='' -E  "s/(__version__ = ').*$/\1$BASE.$NOW'/" ./pywb/version.py 
 git tag v-$BASE.$NOW
-git commit ./pywb/version.py
+git commit -m "version: update to $BASE.$NOW" ./pywb/version.py
+git push
 git push origin v-$BASE.$NOW
