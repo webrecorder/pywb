d2467d5fad7fa139044bc73b28acd09e025f6c92
Ilya Kreymer <ikreymer@gmail.com>
2019-07-02 16:22:08 -0700

    wombat + tests - add build-wombat.sh for building wombat - fix tests (no more karma tests, now in wombat) - update to latest wombat

--- .travis/test.sh
+++ .travis/test.sh
@@ -3,7 +3,6 @@ set -e
 
 if [ "$WR_TEST" = "no" ]; then
     python setup.py test
-    cd karma-tests && make test && cd ..
 else
     cd webrecorder-tests
     INTRAVIS=1 pytest -m "pywbtest and chrometest"
