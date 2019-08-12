3ffb4c21083e52f323e42e4597101f1a775da527
Ilya Kreymer <ikreymer@gmail.com>
2019-08-12 10:23:39 -0700

    update wombat

--- build-wombat.sh
+++ build-wombat.sh
@@ -5,5 +5,5 @@ CURR_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $CURR_DIR/wombat
 export OUTPUT_DIR=../pywb/static/
 yarn install
-yarn run build-dev
+yarn run build-prod
 #cp ./dist/*.js ../pywb/static/
