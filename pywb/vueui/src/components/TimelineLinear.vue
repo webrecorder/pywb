fa317deeb48052fb26d78a37f9364fd943fc4db7
Ivan Velev <vanecat@gmail.com>
2021-09-28 00:56:54 -0700

    vueui: check current snapshot first when using its props

--- pywb/vueui/src/components/TimelineLinear.vue
+++ pywb/vueui/src/components/TimelineLinear.vue
@@ -8,7 +8,7 @@
   <div class="list">
     <div v-for="period in snapshotPeriods">
       <span class="link" @click="gotoPeriod(period)" >{{period.snapshot.getTimeFormatted()}}</span>
-      <span v-if="currentSnapshot.id === period.snapshot.id" class="current">current</span>
+      <span v-if="isCurrentSnapshot(period)" class="current">current</span>
     </div>
   </div>
 </div>
@@ -30,6 +30,9 @@ export default {
   methods: {
     gotoPeriod(period) {
       this.$emit('goto-period', period);
+    },
+    isCurrentSnapshot(period) {
+      return this.currentSnapshot && this.currentSnapshot.id === period.snapshot.id;
     }
   }
 }
