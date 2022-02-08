6e7f0216e18549b0ed463cfd70a3b5a7e6899dec
Ivan Velev <vanecat@gmail.com>
2022-02-07 18:40:17 -0800

    vue ui i18n: localized "view capture on {date}"

--- pywb/vueui/src/components/Timeline.vue
+++ pywb/vueui/src/components/Timeline.vue
@@ -2,11 +2,11 @@
     <div class="timeline">
         <div class="period-tooltip" v-show="tooltipPeriod" :style="{left: tooltipPeriodPos.x+'px', top: tooltipPeriodPos.y+'px'}">
             <template v-if="tooltipPeriod">
-              <div v-if="tooltipPeriod.snapshot">View capture on
-                {{ tooltipPeriod.snapshot.getTimeDateFormatted() }}
+              <div v-if="tooltipPeriod.snapshot">
+                {{ $root._('View capture on {date}', {date: tooltipPeriod.snapshot.getTimeDateFormatted()}) }}
               </div>
-              <div v-else-if="tooltipPeriod.snapshotPeriod">View capture on
-                {{ tooltipPeriod.snapshotPeriod.snapshot.getTimeDateFormatted() }}
+              <div v-else-if="tooltipPeriod.snapshotPeriod">
+                {{ $root._('View capture on {date}', {date: tooltipPeriod.snapshotPeriod.snapshot.getTimeDateFormatted()}) }}
               </div>
               <div v-else-if="tooltipPeriod.snapshotCount">
                 {{ tooltipPeriod.snapshotCount }} captures
