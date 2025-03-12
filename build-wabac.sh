6fe5d2e3c6b82821728f52319e9d8c4f88d4d9b2
Ilya Kreymer <ikreymer@gmail.com>
2025-03-12 13:29:03 -0700

    bump wabac to 2.21.4

--- build-wabac.sh
+++ build-wabac.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-WABAC_SW_URL=https://cdn.jsdelivr.net/npm/@webrecorder/wabac@2.21.3/dist/sw.js
+WABAC_SW_URL=https://cdn.jsdelivr.net/npm/@webrecorder/wabac@2.21.4/dist/sw.js
 
 wget "$WABAC_SW_URL" -O ./pywb/static/wabacWorker.js
